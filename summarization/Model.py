a0e3645ef56e4d34fb938f099cea18b438e2a8a4
qingyuan <ae5656ln@163.com>
2017-09-24 20:57:53 +0800

    abstract summarization with  beam search

---
+++ summarization/Model.py
@@ -0,0 +1,189 @@
+__author__ = 'PC-LiNing'
+
+import random
+import numpy as np
+import tensorflow as tf
+import data_util
+from tensorflow.python.layers import core as layers_core
+
+
+class BiGRUModel(object):
+    def __init__(self,
+                 source_vocab_size,
+                 target_vocab_size,
+                 buckets,
+                 state_size,
+                 embedding_size,
+                 max_gradient,
+                 batch_size,
+                 beam_size,
+                 reverse_target_vocab_table,
+                 is_training,
+                 learning_rate):
+
+        self.source_vocab_size = source_vocab_size
+        self.target_vocab_size = target_vocab_size
+        self.buckets = buckets
+        self.batch_size = batch_size
+        self.beam_size = beam_size
+        self.learning_rate = learning_rate
+        self.global_step = tf.Variable(0, trainable=False, name="global_step")
+        self.state_size = state_size
+
+        self.encoder_inputs = tf.placeholder(tf.int32, shape=[self.batch_size, None])
+        self.decoder_inputs = tf.placeholder(tf.int32, shape=[self.batch_size, None])
+        self.decoder_targets = tf.placeholder(tf.int32, shape=[self.batch_size, None])
+        self.encoder_len = tf.placeholder(tf.int32, shape=[self.batch_size])
+        self.decoder_len = tf.placeholder(tf.int32, shape=[self.batch_size])
+
+        encoder_fw_cell = tf.contrib.rnn.GRUCell(state_size)
+        encoder_bw_cell = tf.contrib.rnn.GRUCell(state_size)
+        decoder_cell = tf.contrib.rnn.GRUCell(state_size)
+
+        if is_training:
+            encoder_fw_cell = tf.contrib.rnn.DropoutWrapper(encoder_fw_cell, output_keep_prob=0.50)
+            encoder_bw_cell = tf.contrib.rnn.DropoutWrapper(encoder_bw_cell, output_keep_prob=0.50)
+            decoder_cell = tf.contrib.rnn.DropoutWrapper(decoder_cell, output_keep_prob=0.50)
+
+        with tf.variable_scope("seq2seq", dtype=tf.float32):
+            with tf.variable_scope("encoder"):
+                encoder_emb = tf.get_variable("embedding", [source_vocab_size, embedding_size], initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.01))
+                # encoder_inputs_emb = [batch_size,length,embedding_size]
+                encoder_inputs_emb = tf.nn.embedding_lookup(encoder_emb, self.encoder_inputs)
+                # encoder_outputs = ([batch_size,num_step,state_size],[batch_size,num_step,state_size])
+                # encoder_states = ([batch_size,state_size],[batch_size,state_size])
+                encoder_outputs, encoder_states = tf.nn.bidirectional_dynamic_rnn(encoder_fw_cell, encoder_bw_cell,
+                                                                                  encoder_inputs_emb,sequence_length=self.encoder_len, dtype=tf.float32)
+
+            with tf.variable_scope("init_state"):
+                self.init_state = tf.contrib.layers.fully_connected(tf.concat(encoder_states, 1), state_size)
+                self.init_state.set_shape([self.batch_size, state_size])
+                # att_states = [batch_size,num_step,state_size * 2]
+                self.att_states = tf.concat(encoder_outputs, 2)
+                self.att_states.set_shape([self.batch_size, None, state_size*2])
+
+            with tf.variable_scope("attention"):
+                # num_units = attention layer size or attention depth
+                attention = tf.contrib.seq2seq.BahdanauAttention(state_size, self.att_states, self.encoder_len)
+                att_decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention, state_size)
+
+            with tf.variable_scope("decoder"):
+                # decode
+                decoder_emb = tf.get_variable("embedding", [target_vocab_size, embedding_size], initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.01))
+                decoder_inputs_emb = tf.nn.embedding_lookup(decoder_emb, self.decoder_inputs)
+                helper = tf.contrib.seq2seq.TrainingHelper(decoder_inputs_emb, self.decoder_len)
+                decoder = tf.contrib.seq2seq.BasicDecoder(cell=att_decoder_cell,
+                                                          helper=helper,
+                                                          initial_state=att_decoder_cell.zero_state(batch_size, tf.float32).clone(cell_state=self.init_state),
+                                                          output_layer=layers_core.Dense(self.target_vocab_size))
+                # outputs = (rnn_output,sample_id)
+                # rnn_output = [batch_size,length,state_size],sample_id = [batch_size,length]
+                # final_state =
+                # final_length = [batch_size,]
+                outputs, final_state, _ = tf.contrib.seq2seq.dynamic_decode(decoder)
+                # outputs_logits = [batch_size,length,state_size]
+                outputs_logits = outputs[0]
+                self.outputs = outputs_logits
+                # weights = [batch_size,length]
+                weights = tf.sequence_mask(self.decoder_len, dtype=tf.float32)
+                # outputs_logits = [batch_size,length,num_decoder_symbols]
+                # docoder_targets = [batch_size,length]
+                # loss_t = [batch_size,length]
+                loss_t = tf.contrib.seq2seq.sequence_loss(outputs_logits, self.decoder_targets, weights,
+                                                          average_across_timesteps=False, average_across_batch=False)
+                self.loss = tf.reduce_sum(loss_t) / self.batch_size
+                params = tf.trainable_variables()
+                opt = tf.train.AdadeltaOptimizer(self.learning_rate, epsilon=1e-6)
+                gradients = tf.gradients(self.loss, params)
+                clipped_gradients, norm = tf.clip_by_global_norm(gradients, max_gradient)
+                self.updates = opt.apply_gradients(zip(clipped_gradients, params), global_step=self.global_step)
+                tf.summary.scalar('loss', self.loss)
+
+                # Inference
+                # tile inputs for beam search decoder
+                enc_outputs = tf.contrib.seq2seq.tile_batch(self.att_states, multiplier=self.beam_size)
+                # add attention,attention is just a computation,no parameters.
+                inference_length = tf.contrib.seq2seq.tile_batch(self.decoder_len, multiplier=self.beam_size)
+                inference_initial_state = tf.contrib.seq2seq.tile_batch(self.init_state, multiplier=self.beam_size)
+                inference_attention = tf.contrib.seq2seq.BahdanauAttention(state_size, enc_outputs, inference_length)
+                inference_decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, inference_attention, state_size)
+                start_tokens = tf.fill([self.batch_size], data_util.ID_GO)
+                end_token = data_util.ID_EOS
+                inference_decoder = tf.contrib.seq2seq.BeamSearchDecoder(
+                    cell=inference_decoder_cell,
+                    embedding=decoder_emb,
+                    start_tokens=start_tokens,
+                    end_token=end_token,
+                    initial_state=inference_decoder_cell.zero_state(batch_size*self.beam_size, tf.float32).clone(cell_state=inference_initial_state),
+                    beam_width=self.beam_size,
+                    output_layer=layers_core.Dense(self.target_vocab_size),
+                    length_penalty_weight=0.0
+                )
+                predict_inference_output, context_state, _ = tf.contrib.seq2seq.dynamic_decode(
+                    decoder=inference_decoder, impute_finished=False)
+                # translations = [length, batch_size, beam_width]
+                translations = predict_inference_output.predicted_ids
+                # prediction_ids = [length, batch_size]
+                prediction_ids = translations[:, :, 0]
+                # prediction_ids = [batch_size,length]
+                prediction_ids = tf.transpose(prediction_ids, perm=[1, 0])
+                # index look up
+                # prediction_words = [batch_size,length]
+                self.prediction_words = reverse_target_vocab_table.lookup(tf.to_int64(prediction_ids))
+
+        self.saver = tf.train.Saver(tf.global_variables(), max_to_keep=0)
+        self.summary_merge = tf.summary.merge_all()
+
+    def step(self, session, encoder_inputs, decoder_inputs, encoder_len, decoder_len, is_training, summary_writer):
+        if encoder_inputs.shape[1] != max(encoder_len):
+            raise ValueError("encoder_inputs and encoder_len does not fit")
+        if decoder_inputs.shape[1] != max(decoder_len) + 1:
+            raise ValueError("decoder_inputs and decoder_len does not fit")
+        input_feed = {}
+        input_feed[self.encoder_inputs] = encoder_inputs
+        input_feed[self.decoder_inputs] = decoder_inputs[:, :-1]
+        input_feed[self.decoder_targets] = decoder_inputs[:, 1:]
+        input_feed[self.encoder_len] = encoder_len
+        input_feed[self.decoder_len] = decoder_len
+        # training
+        if is_training:
+            step, merge, loss, _ = session.run([self.global_step, self.summary_merge, self.loss, self.updates], input_feed)
+        else:
+            step, merge, loss = session.run([self.global_step, self.summary_merge, self.loss], input_feed)
+        if summary_writer:
+            summary_writer.add_summary(merge, step)
+        return step, loss
+
+    def inference(self, session, encoder_inputs, encoder_len):
+        input_feed = {}
+        input_feed[self.encoder_inputs] = encoder_inputs
+        input_feed[self.encoder_len] = encoder_len
+        step, outputs = session.run([self.global_step, self.prediction_words], input_feed)
+        return step, outputs
+
+    def add_pad(self, data, fixlen):
+        data = map(lambda x: x + [data_util.ID_PAD] * (fixlen - len(x)), data)
+        data = list(data)
+        return np.asarray(data)
+
+    def get_batch(self, data, bucket_id):
+        encoder_inputs, decoder_inputs = [], []
+        encoder_len, decoder_len = [], []
+        # Get a random batch of encoder and decoder inputs from data,
+        # and add GO to decoder.
+        for _ in range(self.batch_size):
+            encoder_input, decoder_input = random.choice(data[bucket_id])
+            encoder_inputs.append(encoder_input)
+            encoder_len.append(len(encoder_input))
+            decoder_inputs.append(decoder_input)
+            decoder_len.append(len(decoder_input))
+
+        batch_enc_len = max(encoder_len)
+        batch_dec_len = max(decoder_len)
+        encoder_inputs = self.add_pad(encoder_inputs, batch_enc_len)
+        decoder_inputs = self.add_pad(decoder_inputs, batch_dec_len)
+        encoder_len = np.asarray(encoder_len)
+        # decoder_input has both <GO> and <EOS>
+        # len(decoder_input)-1 is number of steps in the decoder.
+        decoder_len = np.asarray(decoder_len) - 1
+        return encoder_inputs, decoder_inputs, encoder_len, decoder_len
---
+++ summarization/summarization.py
@@ -0,0 +1,209 @@
+__author__ = 'PC-LiNing'
+
+import logging
+import math
+import os
+import random
+import sys
+import time
+import numpy as np
+import tensorflow as tf
+import Model
+import data_util
+
+
+tf.app.flags.DEFINE_float("learning_rate", 1., "Learning rate.")
+tf.app.flags.DEFINE_integer("size", 200, "Size of hidden layers.")
+tf.app.flags.DEFINE_integer("embsize", 200, "Size of embedding.")
+tf.app.flags.DEFINE_integer("num_layers", 1, "Number of layers in the model.")
+tf.app.flags.DEFINE_string("data_dir", "data", "Data directory")
+tf.app.flags.DEFINE_string("test_file", "test.article.txt", "Test filename.")
+tf.app.flags.DEFINE_string("test_output", "output.txt", "Test output.")
+tf.app.flags.DEFINE_string("train_dir", "model", "Training directory.")
+tf.app.flags.DEFINE_string("tfboard", "tfboard", "Tensorboard log directory.")
+tf.app.flags.DEFINE_boolean("decode", False, "Set to True for testing.")
+tf.app.flags.DEFINE_boolean("geneos", True, "Do not generate EOS. ")
+tf.app.flags.DEFINE_float("max_gradient", 1.0, "Clip gradients l2 norm to this range.")
+tf.app.flags.DEFINE_integer("batch_size", 80, "Batch size in training")
+tf.app.flags.DEFINE_integer("beam_size", 5, "beam size in testing.")
+tf.app.flags.DEFINE_integer("doc_vocab_size", 30000, "Document vocabulary size.")
+tf.app.flags.DEFINE_integer("sum_vocab_size", 30000, "Summary vocabulary size.")
+tf.app.flags.DEFINE_integer("max_train", 0, "Limit on the size of training data (0: no limit).")
+tf.app.flags.DEFINE_integer("max_iter", 200000, "Maximum training iterations.")
+tf.app.flags.DEFINE_integer("steps_per_validation", 1000, "Training steps between validations.")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 10000, "Training steps between checkpoints.")
+tf.app.flags.DEFINE_string("checkpoint", "", "Checkpoint to load (use up-to-date if not set)")
+
+FLAGS = tf.app.flags.FLAGS
+
+# We use a number of buckets for sampling
+_buckets = [(30, 10), (50, 20), (70, 20), (100, 20), (200, 30)]
+
+
+# add <GO> and <EOS>
+def create_bucket(source, target):
+    data_set = [[] for _ in _buckets]
+    for s, t in zip(source, target):
+        t = [data_util.ID_GO] + t + [data_util.ID_EOS]
+        for bucket_id, (s_size, t_size) in enumerate(_buckets):
+            if len(s) <= s_size and len(t) <= t_size:
+                data_set[bucket_id].append([s, t])
+                break
+    return data_set
+
+
+def create_model(session, reverse_target_vocab_table, is_training):
+    """Create model and initialize or load parameters in session."""
+    model = Model.BiGRUModel(
+        FLAGS.doc_vocab_size,
+        FLAGS.sum_vocab_size,
+        _buckets,
+        FLAGS.size,
+        FLAGS.embsize,
+        FLAGS.max_gradient,
+        FLAGS.batch_size,
+        FLAGS.beam_size,
+        reverse_target_vocab_table,
+        is_training,
+        FLAGS.learning_rate)
+    if FLAGS.checkpoint != "":
+        ckpt = FLAGS.checkpoint
+    else:
+        ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
+        if ckpt:
+            ckpt = ckpt.model_checkpoint_path
+    if ckpt and tf.train.checkpoint_exists(ckpt):
+        logging.info("Reading model parameters from %s" % ckpt)
+        model.saver.restore(session, ckpt)
+    else:
+        logging.info("Created model with fresh parameters.")
+        session.run(tf.global_variables_initializer())
+    return model
+
+
+def train():
+    logging.info("Preparing summarization data.")
+    docid, sumid, doc_dict, sum_dict = data_util.load_data(FLAGS.data_dir + "/train.article.txt",
+                                                           FLAGS.data_dir + "/train.title.txt",
+                                                           FLAGS.data_dir + "/doc_dict.txt",
+                                                           FLAGS.data_dir + "/sum_dict.txt",
+                                                           FLAGS.doc_vocab_size, FLAGS.sum_vocab_size)
+
+    val_docid, val_sumid = data_util.load_valid_data(FLAGS.data_dir + "/valid.article.filter.txt",
+                                                     FLAGS.data_dir + "/valid.title.filter.txt",
+                                                     doc_dict, sum_dict)
+
+    with tf.Session() as sess:
+        # Create model.
+        logging.info("Creating %d layers of %d units." % (FLAGS.num_layers, FLAGS.size))
+        train_writer = tf.summary.FileWriter(FLAGS.tfboard, sess.graph)
+        # create reverse table
+        reverse_table = tf.contrib.lookup.index_to_string_table_from_file(vocabulary_file="sum_ordered_words.txt",
+                                                                          default_value="<UNK>")
+        model = create_model(sess, reverse_table, is_training=True)
+
+        # Read data into buckets and compute their sizes.
+        logging.info("Create buckets.")
+        dev_set = create_bucket(val_docid, val_sumid)
+        train_set = create_bucket(docid, sumid)
+
+        train_bucket_sizes = [len(train_set[b]) for b in range(len(_buckets))]
+        train_total_size = float(sum(train_bucket_sizes))
+        train_buckets_scale = [sum(train_bucket_sizes[:i + 1])/train_total_size for i in range(len(train_bucket_sizes))]
+
+        for (s_size, t_size), nsample in zip(_buckets, train_bucket_sizes):
+            logging.info("Train set bucket ({}, {}) has {} samples.".format(s_size, t_size, nsample))
+
+        # This is the training loop.
+        step_time, loss = 0.0, 0.0
+        current_step = sess.run(model.global_step)
+        while current_step <= FLAGS.max_iter:
+            random_number_01 = np.random.random_sample()
+            bucket_id = min([i for i in range(len(train_buckets_scale)) if train_buckets_scale[i] > random_number_01])
+            # Get a batch and make a step.
+            start_time = time.time()
+            encoder_inputs, decoder_inputs, encoder_len, decoder_len = model.get_batch(train_set, bucket_id)
+            current_step, step_loss = model.step(sess, encoder_inputs, decoder_inputs, encoder_len, decoder_len,
+                                                 is_training=True, summary_writer=train_writer)
+            step_time += (time.time() - start_time) / FLAGS.steps_per_validation
+            loss += step_loss * FLAGS.batch_size / np.sum(decoder_len) / FLAGS.steps_per_validation
+            # Once in a while, we save checkpoint.
+            if current_step % FLAGS.steps_per_checkpoint == 0:
+                # Save checkpoint and zero timer and loss.
+                checkpoint_path = os.path.join(FLAGS.train_dir, "model.ckpt")
+                model.saver.save(sess, checkpoint_path, global_step=model.global_step)
+
+            # Once in a while, we print statistics and run evals.
+            if current_step % FLAGS.steps_per_validation == 0:
+                # Print statistics for the previous epoch.
+                perplexity = np.exp(float(loss))
+                logging.info("global step %d step-time %.2f ppl %.2f" % (model.global_step.eval(), step_time, perplexity))
+
+                # Run evals on development set and print their perplexity.
+                step_time, loss = 0.0, 0.0
+                for bucket_id in range(len(_buckets)):
+                    if len(dev_set[bucket_id]) == 0:
+                        logging.info("  eval: empty bucket %d" % (bucket_id))
+                        continue
+                    encoder_inputs, decoder_inputs, encoder_len, decoder_len = model.get_batch(dev_set, bucket_id)
+                    eval_step, eval_loss = model.step(sess, encoder_inputs, decoder_inputs, encoder_len, decoder_len,
+                                                      is_training=False, summary_writer=None)
+                    eval_loss = eval_loss * FLAGS.batch_size / np.sum(decoder_len)
+                    eval_ppx = np.exp(float(eval_loss))
+                    logging.info("  eval: bucket %d ppl %.2f" % (bucket_id, eval_ppx))
+                sys.stdout.flush()
+
+
+def decode():
+    # Load vocabularies.
+    doc_dict = data_util.load_dict(FLAGS.data_dir + "/doc_dict.txt")
+    sum_dict = data_util.load_dict(FLAGS.data_dir + "/sum_dict.txt")
+    if doc_dict is None or sum_dict is None:
+        logging.warning("Dict not found.")
+    data = data_util.load_test_data(FLAGS.test_file, doc_dict)
+
+    with tf.Session() as sess:
+        # Create model and load parameters.
+        logging.info("Creating %d layers of %d units." % (FLAGS.num_layers, FLAGS.size))
+        # create reverse table
+        reverse_table = tf.contrib.lookup.index_to_string_table_from_file(vocabulary_file="sum_ordered_words.txt",
+                                                                          default_value="<UNK>")
+        model = create_model(sess, reverse_table, is_training=False)
+        result = []
+        for idx, token_ids in enumerate(data):
+            # Get a 1-element batch to feed the sentence to the model.
+            encoder_inputs, decoder_inputs, encoder_len, decoder_len = model.get_batch(
+                    {0: [(token_ids, [data_util.ID_GO, data_util.ID_EOS])]}, 0)
+            # repeat
+            if encoder_inputs.shape[0] == 1:
+                encoder_inputs = np.repeat(encoder_inputs, FLAGS.batch_size, axis=0)
+                encoder_len = np.repeat(encoder_len, FLAGS.batch_size, axis=0)
+            # outputs = [batch_size,length]
+            step, outputs = model.inference(sess, encoder_inputs, encoder_len)
+            # If there is an EOS symbol in outputs, cut them at that point.
+            target_output = outputs[0]
+            if data_util.ID_EOS in target_output:
+                target_output = target_output[:target_output.index(data_util.ID_EOS)]
+            gen_sum = " ".join(target_output)
+            result.append(gen_sum)
+            logging.info("Finish {} samples. :: {}".format(idx, gen_sum[:75]))
+        with open(FLAGS.test_output, "w") as f:
+            for item in result:
+                print(item, file=f)
+
+
+def main(_):
+    if FLAGS.decode:
+        decode()
+    else:
+        train()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s",
+                        datefmt='%b %d %H:%M')
+    try:
+        os.makedirs(FLAGS.train_dir)
+    except:
+        pass
+    tf.app.run()
